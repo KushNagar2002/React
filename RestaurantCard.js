556a1fce6e72ebe83c14e402efc2305bc13c1276
KushNagar2002 <155545416+KushNagar2002@users.noreply.github.com>
2024-06-21 11:30:24 +0530

    Update RestaurantCard.js

--- RestaurantCard.js
+++ RestaurantCard.js
@@ -1,17 +1,20 @@
 import { CDN_URL } from "../utils/constants";
 
 const RestaurantCard = ({resData}) => {
-    const { name, cuisines, avgRating } = resData.info;
+    const { name, cuisines, avgRating, cloudinaryImageId, locality, costForTwo} = resData.info;
+
     return (
         <div className="res-card">
             <img 
             className="res-logo" 
             alt="res-logo"
-            src={CDN_URL}
+            src={CDN_URL + cloudinaryImageId} 
             ></img>
             <h3>{name}</h3>
             <h4>{cuisines.join(', ')}</h4>
             <h4>{avgRating}</h4>
+            <h4>{locality}</h4>
+            <h4>{costForTwo}</h4>
         </div>
     );
 };
